3d3243e42b6f3c654742e4f6cd5fa2655162d375
Aaron Jacobs <aaron@carnotinteractive.com>
2013-01-26 19:37:39 -0800

    Added simple enemy AI

--- src/edu/calpoly/csc/pulseman/gameobject/Enemy.java
+++ src/edu/calpoly/csc/pulseman/gameobject/Enemy.java
@@ -1,13 +1,18 @@
 package edu.calpoly.csc.pulseman.gameobject;
 
+import java.util.List;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Rectangle;
 
+import edu.calpoly.csc.pulseman.World;
+
 public class Enemy extends Entity implements Murderer
 {
 	private static Image image;
+	private Direction direction;
 
 	public static void init(Image image)
 	{
@@ -17,6 +22,7 @@ public class Enemy extends Entity implements Murderer
 	public Enemy(int x, int y)
 	{
 		super(new Rectangle(x, y, image.getWidth(), image.getHeight()));
+		direction = Direction.LEFT;
 	}
 
 	public void render(GameContainer gc, Graphics g)
@@ -26,6 +32,68 @@ public class Enemy extends Entity implements Murderer
 
 	public void update(GameContainer gc, int delta)
 	{
+		if(direction == Direction.LEFT)
+		{
+			boolean leftIntersect = false, bottomLeftIntersect = false;
+			Rectangle left = new Rectangle(bounds.getX() - (bounds.getWidth() / 6), bounds.getY() - (bounds.getHeight() / 4), bounds.getWidth(), bounds.getHeight());
+			Rectangle bottomLeft = new Rectangle(left.getX(), left.getY() + left.getHeight(), left.getWidth(), left.getHeight());
+
+			List<Collidable> collidables = World.getWorld().getCollidables();
+			for(int i = 0; i < collidables.size(); ++i)
+			{
+				if(left.intersects(collidables.get(i).bounds))
+				{
+					leftIntersect = true;
+					break;
+				}
+
+				if(bottomLeft.intersects(collidables.get(i).bounds))
+				{
+					bottomLeftIntersect = true;
+				}
+			}
+
+			if(leftIntersect || (!bottomLeftIntersect && floor != null))
+			{
+				direction = Direction.RIGHT;
+			}
+		}
+		else
+		{
+			boolean rightIntersect = false, bottomRightIntersect = false;
+			Rectangle right = new Rectangle(bounds.getX() + (bounds.getWidth() / 6), bounds.getY() - (bounds.getHeight() / 4), bounds.getWidth(), bounds.getHeight());
+			Rectangle bottomRight = new Rectangle(right.getX(), right.getY() + right.getHeight(), right.getWidth(), right.getHeight());
+
+			List<Collidable> collidables = World.getWorld().getCollidables();
+			for(int i = 0; i < collidables.size(); ++i)
+			{
+				if(right.intersects(collidables.get(i).bounds))
+				{
+					rightIntersect = true;
+					break;
+				}
+
+				if(bottomRight.intersects(collidables.get(i).bounds))
+				{
+					bottomRightIntersect = true;
+				}
+			}
+
+			if(rightIntersect || (!bottomRightIntersect && floor != null))
+			{
+				direction = Direction.LEFT;
+			}
+		}
+
+		if(direction == Direction.LEFT)
+		{
+			position.x -= Player.PLAYER_SPEED * delta;
+		}
+		else
+		{
+			position.x += Player.PLAYER_SPEED * delta;
+		}
+
 		super.update(gc, delta);
 	}
 }
