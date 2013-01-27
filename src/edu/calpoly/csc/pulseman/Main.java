6b460d928c13c72642a2b98b6c0876daf97cd16d
wallisc <cwallis@calpoly.edu>
2013-01-27 15:13:07 -0800

    Fixed spikes FOR THE LAST DAMN TIME

--- src/edu/calpoly/csc/pulseman/Main.java
+++ src/edu/calpoly/csc/pulseman/Main.java
@@ -31,7 +31,7 @@ public class Main extends BasicGame
 	private static GameState state = GameState.MENU;
 	private static int curLevel = -1;
 	private static String[] levels =
-	{ "res/level1.png", "res/level2.png", "res/level3.png", "res/level4.png",
+	{ "res/level6.png", "res/level2.png", "res/level3.png", "res/level4.png",
 	  "res/level5.png", "res/level6.png"};
 	private static final int width = 1280, height = 720;
 	private static volatile int androidState = AndroidStates.NOT_CONNECTED;
