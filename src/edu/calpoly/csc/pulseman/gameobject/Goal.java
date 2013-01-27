a0c507a781b496e3b0ca71f91dc4c8afdbc31379
wallisc <cwallis@calpoly.edu>
2013-01-26 17:44:44 -0800

    Fixed player animation and made the portal a fire

--- src/edu/calpoly/csc/pulseman/gameobject/Goal.java
+++ src/edu/calpoly/csc/pulseman/gameobject/Goal.java
@@ -3,13 +3,20 @@ package edu.calpoly.csc.pulseman.gameobject;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.particles.ParticleEmitter;
+import org.newdawn.slick.particles.ParticleSystem;
+import org.newdawn.slick.particles.effects.FireEmitter;
 
 public class Goal extends Collidable {
-	public static Image image;
+	private static Image image;
+	private ParticleSystem ps;
 	
-	public Goal(int x, int y) {
+	public Goal(int x, int y) throws SlickException {
 		super(new Rectangle(x, y, image.getWidth(), image.getHeight()));
+		ps = new ParticleSystem(new Image("res/orb.png"));
+		ps.addEmitter(new FireEmitter());
 	}
 
 	public static void init(Image image) {
@@ -18,12 +25,14 @@ public class Goal extends Collidable {
 	
 	@Override
 	public void render(GameContainer gc, Graphics g) {
-		g.drawImage(image, getHitBox().getX(), getHitBox().getY());
+		//g.drawImage(image, getHitBox().getX(), getHitBox().getY());
+		ps.render(getHitBox().getX() + getHitBox().getWidth() / 2.0f, 
+				getHitBox().getY() + getHitBox().getHeight() / 2.0f);
 	}
 
 	@Override
 	public void update(GameContainer gc, int delta) {
-		
+		ps.update(delta);
 	}
 
 }
