edaeeea59246eeb44ad41e50b054fea7f39ef061
Aaron Jacobs <aaron@carnotinteractive.com>
2013-01-27 11:15:59 -0800

    Added levels 3 and 4 to main

--- src/edu/calpoly/csc/pulseman/Main.java
+++ src/edu/calpoly/csc/pulseman/Main.java
@@ -12,7 +12,6 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.Sound;
 
 import edu.calpoly.csc.pulseman.MessageHandler.MessageReceiver;
 
@@ -32,7 +31,7 @@ public class Main extends BasicGame
 	private static GameState state = GameState.MENU;
 	private static int curLevel = -1;
 	private static String[] levels =
-	{ "res/level1.png", "res/level2.png"};//, "res/level2.png", "res/level3.png", "res/level4.png", "res/level5.png", "res/level6.png", "res/level7.png", "res/level8.png", "res/level9.png", "res/level10.png" };
+	{ "res/level1.png", "res/level2.png", "res/level3.png", "res/level4.png"};
 	private static final int width = 1280, height = 720;
 	private static volatile int androidState = AndroidStates.NOT_CONNECTED;
 	Map<GameState, GameInterface> interfaceMap = new HashMap<GameState, GameInterface>();
