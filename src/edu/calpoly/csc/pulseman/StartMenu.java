61b7a31cd24d7886e09c8ce1b727cc706c733ebc
Aaron Jacobs <aaron@carnotinteractive.com>
2013-03-15 01:01:28 -0700

    Started work on story pages for menu

--- src/edu/calpoly/csc/pulseman/StartMenu.java
+++ src/edu/calpoly/csc/pulseman/StartMenu.java
@@ -3,6 +3,7 @@ package edu.calpoly.csc.pulseman;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -11,6 +12,15 @@ import org.newdawn.slick.SlickException;
 
 public class StartMenu implements GameInterface
 {
+	private class MenuState
+	{
+		public static final int MAIN = 0,
+				STORY_1 = 1,
+				STORY_2 = 2;
+	}
+	
+	private int state = MenuState.MAIN;
+	
 	private Image menuButton;
 	private Image menuBackground;
 	private Image connectButton, connectingButton, connectedButton;
@@ -24,29 +34,40 @@ public class StartMenu implements GameInterface
 	@Override
 	public void render(GameContainer gc, Graphics g)
 	{
-		g.drawImage(menuBackground, 0, 0);
-		g.drawImage(menuButton, buttonLoc[0], buttonLoc[1]);
+		if(state == MenuState.MAIN)
+		{
+			g.drawImage(menuBackground, 0, 0);
+			g.drawImage(menuButton, buttonLoc[0], buttonLoc[1]);
 
-		if(Main.getAndroidState() == Main.AndroidStates.NOT_CONNECTED)
-		{
-			g.drawImage(connectButton, connectLoc[0], connectLoc[1]);
-		}
-		else if(Main.getAndroidState() == Main.AndroidStates.CONNECTING)
-		{
-			g.drawImage(connectingButton, connectLoc[0], connectLoc[1]);
+			g.setColor(Color.white);
+			if(Main.getAndroidState() == Main.AndroidStates.NOT_CONNECTED)
+			{
+				g.drawImage(connectButton, connectLoc[0], connectLoc[1]);
+			}
+			else if(Main.getAndroidState() == Main.AndroidStates.CONNECTING)
+			{
+				g.drawImage(connectingButton, connectLoc[0], connectLoc[1]);
 
-			g.drawString(String.valueOf(countdown), connectLoc[0] + connectingButton.getWidth(), connectLoc[1] + connectingButton.getHeight() / 2);
-		}
-		else
-		{
-			g.drawImage(connectedButton, connectLoc[0], connectLoc[1]);
-		}
+				g.drawString(String.valueOf(countdown), connectLoc[0] + connectingButton.getWidth(), connectLoc[1] + connectingButton.getHeight() / 2);
+			}
+			else
+			{
+				g.drawImage(connectedButton, connectLoc[0], connectLoc[1]);
+			}
 
-		if(ip != null)
-		{
-			g.drawString("Your IP address is: " + ip, 100, 100);
+			if(ip != null)
+			{
+				g.drawString("Your IP address is: " + ip, 100, 100);
+			}
 		}
-		//g.drawString("You are a meditating monk. Head towards the light.\n" + "Use the beat to control nature's speed.", Main.getScreenWidth() / 2, Main.getScreenHeight() / 2);
+		else if(state == MenuState.STORY_1)
+		{
+			//TODO
+		}
+		else if(state == MenuState.STORY_2)
+		{
+			//TODO
+		}
 	}
 
 	@Override
@@ -77,6 +98,7 @@ public class StartMenu implements GameInterface
 			if(Main.getAndroidState() == Main.AndroidStates.NOT_CONNECTED && (x >= connectLoc[0] && x <= connectLoc[0] + connectButton.getWidth() && y >= connectLoc[1] && y <= connectLoc[1] + connectButton.getHeight()))
 			{
 				listenForConnection();
+				state = MenuState.STORY_1;
 
 				countdown = MessageHandler.SOCKET_TIMEOUT / 1000 + 1;
 				new Timer("Countdown Timer").schedule(new TimerTask()
